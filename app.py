98fcb1b3511124ce465f07e4ff750831cb06ee77
Sidharth M S <kidsachu@gmail.com>
2023-04-18 19:15:23 +0530

    upload

---
+++ app.py
@@ -0,0 +1,67 @@
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import cv2
+import numpy as np
+
+UPLOAD_FOLDER = r'C:/Users/Sidharth/Documents/code/ml project/shots'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = "random string"
+
+from keras.models import Model, load_model
+new_model=load_model('rotten.h5')
+
+def cat(x):
+    val = x[0][0]
+    print(val)
+    if val>.5: return "Rotten"
+    else : return "Fresh"
+
+def pred(img):
+    img = cv2.imread(os.path.join("shots",img))
+    print(img)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img,(100,100))
+    print(cat(new_model.predict(img.reshape(1,100,100,3))))
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            print(filename)
+            pred(filename)
+            # img = cv2.imdecode(np.fromstring(file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
+            return redirect("/")
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>'''
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
